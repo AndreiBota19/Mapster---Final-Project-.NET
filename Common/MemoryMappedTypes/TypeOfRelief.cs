a0b0a6a95a0c22bcd2df0e212b5fcd0fea5c1108
Andrei Bota <andreibota00@gmail.com>
2022-05-27 20:06:22 +0300

    Final Project

---
+++ Common/MemoryMappedTypes/TypeOfRelief.cs
@@ -0,0 +1,20 @@
+﻿namespace Mapster.Rendering;
+
+public enum TypeOfRelief : int
+{
+    name,
+    landuse,
+    highway,
+    water,
+    boundary ,
+    natural ,
+    leisure ,
+    amenity ,
+    building ,
+    admin_level,
+    place,
+    railway,
+    
+
+
+}
